986eb6534763291a8acbf02dd39d445e9f9a81c4
Suthankan Balenthiran <Suthankanbala2019@gmail.com>
2026-09-18 14:03:03 +0530

    security: remove unused alb https ingress

--- infrastructure/terraform/environments/dev/security.tf
+++ infrastructure/terraform/environments/dev/security.tf
@@ -54,17 +54,6 @@ resource "aws_vpc_security_group_ingress_rule" "alb_http" {
   ip_protocol = "tcp"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "alb_https" {
-  security_group_id = aws_security_group.alb.id
-
-  description = "Allow public HTTPS traffic."
-
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 443
-  to_port     = 443
-  ip_protocol = "tcp"
-}
-
 resource "aws_vpc_security_group_egress_rule" "alb_to_station" {
   security_group_id = aws_security_group.alb.id
 
